e6d1f72ac32ed8a26a8f68bccb56d05e1af890bf
samdevo <samdevorsetz@gmail.com>
2015-08-19 18:02:06 +0000

    merge fix

---
+++ app/models/tweet.rb
@@ -0,0 +1,10 @@
+class Tweet < ActiveRecord::Base
+#   #username, message
+#   attr_accessor :username, :message
+#   def initialize(username, message)
+#     @username = username
+#     @message = message
+#   end
+  
+
+end
