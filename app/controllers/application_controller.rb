8ceb8bedab6475913e0ffc8c531ff53fe32c2ba5
samdevo <samdevorsetz@gmail.com>
2015-08-24 17:19:36 +0000

    error fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,21 +5,25 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, "public"
     set :views, "app/views"
+    enable :sessions
+    set :session_secret, "flatironizcool"
   end
   get "/" do
-    @all_yaks = Yak.all
+    @yaks = Yak.all
     erb :signin
   end
   post '/signin' do
+    session[:user_id] = 
     redirect '/index'
   end
   get "/index" do
+    @yaks = Yak.all
     erb :index
   end
   post "/new_yak" do
     @yak = Yak.new({:user => params[:user], :message => params[:message]})
     @yak.save
-    @all_yaks = Yak.all
+    @yaks = Yak.all
     erb :index
   end
   post '/delete_yak' do
